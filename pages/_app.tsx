5dbd20dc483b7c0c47fdb58af2a5fe4536785b0e
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-26 12:38:37 -0800

    Nav menu
    - Nav menu scales in with an animation
    - Nav links to pages that are planned for
    - Nav links have a hover animation
    - Added fontawesome icons as a dependency
    - Used an icon for the menu toggle button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,26 +3,28 @@ import type { AppProps } from 'next/app';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../public/la buca.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function App({ Component, pageProps }: AppProps) {
   const toggleNav = () => {
     const nav = document.getElementById("nav");
-    if (nav) nav.classList.toggle("hidden");
+    if (nav) { nav.classList.toggle("scale-0"); nav.classList.toggle("scale-100") };
   }
 
   return (
     <>
-      <header className='bg-slate-600 flex justify-between align-middle p-4'>
+      <header className='bg-slate-600 flex justify-between align-middle p-4 relative'>
         <Link href="/">
           <Image src={logo} alt="la buca" />
         </Link>
-        <button type="button" onClick={toggleNav}>
-          O
+        <button type="button" onClick={toggleNav} className="z-10 border-none bg-transparent">
+          <FontAwesomeIcon icon={faBars} className="text-2xl hover:scale-110 transition-all" />
         </button>
-        <nav id="nav" className='hidden'>
-          <Link href="/menu">Menu</Link>
-          <Link href="/catering">Catering</Link>
-          <Link href="/contact">Contact</Link>
+        <nav id="nav" className='scale-0 absolute flex flex-col align-middle top-0 right-0 bg-slate-900 px-6 py-3 pt-5 w-40 transition-all origin-top-right shadow-lg'>
+          <Link href="/menu" className="nav-link">Menu</Link>
+          <Link href="/catering" className="nav-link">Catering</Link>
+          <Link href="/contact" className="nav-link">Contact</Link>
         </nav>
       </header>
       <main>
