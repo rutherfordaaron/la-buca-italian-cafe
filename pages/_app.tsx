d7a7d6a8d149f81e17d1ef3577d3d571baa11a17
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-26 12:18:00 -0800

    Header content and initial styles
    Header has a logo and a nav menu. Styles are just initial and will be changing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../public/la buca.png';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const toggleNav = () => {
+    const nav = document.getElementById("nav");
+    if (nav) nav.classList.toggle("hidden");
+  }
+
   return (
     <>
-      <header>
-
+      <header className='bg-slate-600 flex justify-between align-middle p-4'>
+        <Link href="/">
+          <Image src={logo} alt="la buca" />
+        </Link>
+        <button type="button" onClick={toggleNav}>
+          O
+        </button>
+        <nav id="nav" className='hidden'>
+          <Link href="/menu">Menu</Link>
+          <Link href="/catering">Catering</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
       </header>
       <main>
         <Component {...pageProps} />
