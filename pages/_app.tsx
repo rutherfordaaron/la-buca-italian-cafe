0b8483bd1d6ebcf84565c0709c4c2c0c552f5785
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-26 16:34:34 -0800

    Removed image from header
    replaced with text and given a subtle hover state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,14 +14,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <header className='bg-slate-800 flex justify-between align-middle p-4 relative shadow-lg z-10'>
-        <Link href="/">
-          <Image src={logo} alt="la buca" />
-        </Link>
+      <header className='bg-slate-800 flex justify-between items-center p-4 relative shadow-lg z-10'>
+        <Link href="/" className='text-4xl font-bold hover:text-slate-200'>la buca</Link>
         <button type="button" onClick={toggleNav} className="z-10 border-none bg-transparent">
           <FontAwesomeIcon icon={faBars} className="text-2xl hover:scale-110 transition-all" />
         </button>
-        <nav id="nav" className='scale-0 absolute flex flex-col align-middle top-0 right-0 bg-slate-900 px-6 py-3 pt-5 w-40 transition-all origin-top-right shadow-lg'>
+        <nav id="nav" className='scale-0 absolute flex flex-col justify-end top-0 right-0 bg-slate-900 px-6 py-3 pt-5 w-40 transition-all origin-top-right shadow-lg'>
           <Link href="/menu" className="nav-link">Menu</Link>
           <Link href="/catering" className="nav-link">Catering</Link>
           <Link href="/contact" className="nav-link">Contact</Link>
