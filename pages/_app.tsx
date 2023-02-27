5fc93c98292c406ac24474043d82ce716d29bfb7
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-26 17:10:30 -0800

    Minor hover changes in the header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,8 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <header className='bg-slate-800 flex justify-between items-center p-4 relative shadow-lg z-10'>
         <Link href="/" className='text-4xl font-bold hover:text-slate-200'>la buca</Link>
-        <button type="button" onClick={toggleNav} className="z-10 border-none bg-transparent">
-          <FontAwesomeIcon icon={faBars} className="text-2xl hover:scale-110 transition-all" />
+        <button type="button" onClick={toggleNav} className="z-10 border-none bg-transparent hover:scale-110 transition-all">
+          <FontAwesomeIcon icon={faBars} className="text-2xl transition-all" />
         </button>
         <nav id="nav" className='scale-0 absolute flex flex-col justify-end top-0 right-0 bg-slate-900 px-6 py-3 pt-5 w-40 transition-all origin-top-right shadow-lg'>
           <Link href="/menu" className="nav-link">Menu</Link>
