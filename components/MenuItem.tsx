4abe5e384c8c030689f5914ffb14d05ebd608c80
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-01 12:29:08 -0800

    Allow for 2 sizes with 2 different prices

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -16,13 +16,31 @@ export const symbols = {
   "VEGAN": <FontAwesomeIcon icon={faSeedling} />
 }
 
-const MenuItem = (props: { name: string, description: string, tags: Tag[], price: number }) => {
+const MenuItem = (props: { name: string, description: string, tags: Tag[], price: number | [number, number], sizes?: [string, string] }) => {
   const { name, description, tags, price } = props;
+
+  const getPrices = () => {
+    if (typeof price !== "object") {
+      return (
+        <p className="text-slate-300 font-bold">${price}</p>
+      )
+    } else if (props.sizes) {
+      return (
+        <div className="flex flex-col justify-center items-start text-sm min-w-[25%]">
+          <p className="text-slate-300 font-bold"><span className="italic">{props.sizes[0]}</span> ${price[0]}</p>
+          <p className="text-slate-300 font-bold"><span className="italic">{props.sizes[1]}</span> ${price[1]}</p>
+        </div>
+      )
+    } else {
+      return <div className="text-slate-300 font-bold">???</div>
+    }
+  }
+
   return (
     <div className="my-5">
       <div className="flex justify-between gap-3 items-center">
         <p className="font-bold">{name} <span className="text-slate-400 font-normal">{description}</span></p>
-        <p className="text-slate-300 font-bold">${price}</p>
+        {getPrices()}
       </div>
       <div className="text-slate-500 flex items-center gap-3 text-xs">
         {tags.map((el, i) => {
