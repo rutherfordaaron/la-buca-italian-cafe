9a68414e7fcbbbc7ff8db1e7609285b554df9d1c
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-01 15:15:20 -0800

    Kids menu complete

--- components/Kids.tsx
+++ components/Kids.tsx
@@ -3,8 +3,50 @@ import { Tag } from "./MenuItem";
 
 const Kids = () => {
   return (
-    <section className="min-h-screen p-5">
-
+    <section className="p-5">
+      <p className="italic text-slate-400">All main items served with apple slices and bread</p>
+      <MenuItem
+        name="Bavette Steak"
+        description="and buttered mashed potatoes"
+        price={13}
+        tags={[]}
+      />
+      <MenuItem
+        name="Roasted Chicken"
+        description="and broccoli"
+        price={12}
+        tags={[]}
+      />
+      <MenuItem
+        name="Pasta"
+        description="penne with marinara, alfredo, or butter and parmesan"
+        price={9}
+        tags={[]}
+      />
+      <MenuItem
+        name="Add Chicken"
+        description=""
+        price={5}
+        tags={[]}
+      />
+      <MenuItem
+        name="Add Broccoli"
+        description=""
+        price={4}
+        tags={[]}
+      />
+      <MenuItem
+        name="Add Peas"
+        description=""
+        price={3.5}
+        tags={[]}
+      />
+      <MenuItem
+        name="Add Mashed Potatoes"
+        description="buttered"
+        price={5}
+        tags={[]}
+      />
     </section>
   )
 }
