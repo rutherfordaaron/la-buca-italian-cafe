5f002220c2228f169a3eb010a7bea9868108db0c
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-01 08:05:56 -0800

    Skeleton outline
    Also changed main background color from stone to slate

---
+++ pages/menu.tsx
@@ -0,0 +1,41 @@
+const Menu = () => {
+  return (
+    <article className="min-h-screen p-5">
+      <h1 className="text-center">Menu</h1>
+      <hr className="font-bold" />
+      <section>
+        <h2>Starters</h2>
+      </section>
+
+      <section>
+        <h2>Insalate & Contorni</h2>
+      </section>
+
+      <section>
+        <h2>Pasta</h2>
+      </section>
+
+      <section>
+        <h2>Additions</h2>
+      </section>
+
+      <section>
+        <h2>Etrees</h2>
+      </section>
+
+      <section>
+        <h2>Kids</h2>
+      </section>
+
+      <section>
+        <h2>Beverages</h2>
+      </section>
+
+      <section>
+        <h2>Desserts</h2>
+      </section>
+    </article>
+  )
+}
+
+export default Menu;
